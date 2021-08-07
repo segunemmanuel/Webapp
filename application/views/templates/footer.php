15a1464e81e8ec70f6caa853aa3b8c0caccf418b
segun <segunemmanuel46@gmail.com>
2021-08-06 17:02:58 -0700

    Initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,14 @@
+        </div><!-- end .container -->
+
+        <footer class="text-center" style="color:#777;">
+            <hr>
+            <small>Contact Developer <a href="" target="_blank"> Your Name</a> Your Location </small>
+        </footer><br><br>
+        
+        <!-- jQuery -->
+        <script src="<?php echo base_url("assets//code.jquery.com/jquery-2.1.4.min.js") ?>"></script>
+        
+        <!-- Bootstrap JS -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    </body>
+</html>
